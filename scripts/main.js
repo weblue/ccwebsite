f2960e1c339a9964c12db6721196d77cd88bfd2c
floon <floon@ssh.dreambot.org:floon.git>
2018-08-16 22:55:59 -0400

    first commit for the website

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+const timeBetweenAnim = 3000;
+let titleIndex = 0;
+let titleList = ['Creators\'', 'Programmers\'', 'Photographers\'', 'Producers\'', 'Designers\'', 'Influencers\''];
+
+document.addEventListener("DOMContentLoaded", function(event) {
+        setInterval(() => {
+            let textEle = document.getElementById('animatedLogoText');
+            console.log('animating the stuff');
+            textEle.classList.add("animateFadeOut");
+            setTimeout(() => {
+                nextName(textEle);
+                textEle.classList.remove("animateFadeOut");
+                textEle.classList.add("animateFadeIn");
+            }, 1000);
+        }, timeBetweenAnim);
+});
+
+function nextName(element) {
+    titleIndex++;
+    if (titleIndex === titleList.length) {
+        titleIndex = 0;
+    }
+
+    element.innerText = titleList[titleIndex];
+}
