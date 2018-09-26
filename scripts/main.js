4d1f857af03d0513aed74576c20dce8726f84ecb
floon <floon@ssh.dreambot.org:floon.git>
2018-09-26 00:17:48 -0400

    big updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
+//TODO fix shake
+//TODO animations
+//TODO randomly generate accent color
+//TODO offerings page
+
 const timeBetweenAnim = 3000;
 let titleIndex = 0;
 let titleList = ['Creators\'', 'Programmers\'', 'Photographers\'', 'Producers\'', 'Designers\'', 'Influencers\''];
@@ -22,3 +27,26 @@ function nextName(element) {
 }
 
 setInterval(() => new WOW().init(), 8000);
+
+$(document).ready(() => {
+    goHome();
+});
+
+$('#home_button').click(goHome);
+$('#about_button').click(goAbout);
+$('#contact_button').click(goContact);
+
+function goHome() {
+    $('#small_logo').hide();
+    $('#wrapper').load('home.html');
+}
+
+function goAbout() {
+    $('#small_logo').show();
+    $('#wrapper').load('about.html');
+}
+
+function goContact() {
+    $('#small_logo').show();
+    $('#wrapper').load('contact.html');
+}
